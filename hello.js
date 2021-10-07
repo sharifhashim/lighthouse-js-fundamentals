d47180331dfaf9ae092c99701660ba3f571a101a
Hashim Sharif <sharif.hashim@gmail.com>
2021-10-07 15:24:14 +0000

    added code

--- hello.js
+++ hello.js
@@ -13,3 +13,20 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
 
+/*const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+*/
+
+/*const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+*/
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
